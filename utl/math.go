da609fe54ddb9353e9279a726b01c9dec2add33f
ference <fym201@msn.com>
2018-08-29 20:31:49 +0800

    match

--- utl/math.go
+++ utl/math.go
@@ -22,3 +22,31 @@ func PowInt(x int, y int) int {
 	}
 	return num
 }
+
+func MaxInt64(x, y int64) int64 {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func MinInt64(x, y int64) int64 {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func MaxInt(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func MinInt(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
