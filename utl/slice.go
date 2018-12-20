30fcf03c8f485aed0b1a9fedf8e0e4517cc3f20d
ference <fym201@msn.com>
2018-12-20 20:03:29 +0800

    add IsSliceContainsFloat64

--- utl/slice.go
+++ utl/slice.go
@@ -88,6 +88,15 @@ func IsSliceContainsInt64(sl []int64, i int64) bool {
 	return false
 }
 
+func IsSliceContainsFloat64(sl []float64, i float64) bool {
+	for _, s := range sl {
+		if s == i {
+			return true
+		}
+	}
+	return false
+}
+
 func IsSliceContainsInt(sl []int, i int) bool {
 	for _, s := range sl {
 		if s == i {
